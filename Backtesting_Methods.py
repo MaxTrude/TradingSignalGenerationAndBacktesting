fa9ec8161027f3ec13158d1bfa01d6935e74c668
MaxTrude <max.trude@web.de>
2025-01-20 11:26:05 +0100

    Add files via upload

---
+++ Backtesting_Methods.py
@@ -0,0 +1,668 @@
+import statistics
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from finta import TA as ta
+import yfinance as yf
+import math
+from matplotlib.gridspec import GridSpec
+
+# Generated Relative Strength signals with a restriction -> long entries require a maximum RSI and short entries a minimum
+def rsi_crosses_cap(rsi, sma, u, l):
+    #print(rsi)
+    ret = len(rsi)*[0]
+
+    for i in range(len(rsi)-1):
+        if rsi[i] <= sma[i] and rsi[i+1] >= sma[i+1] and rsi[i+1] > u:
+            ret[i+1] = -1
+        elif rsi[i] >= sma[i] and rsi[i+1] <= sma[i+1] and rsi[i+1] < l:
+            ret[i+1] = 1
+        else:
+            ret[i+1] = 0
+
+    return np.array(ret)
+
+
+
+# Generated Relative Strength signals WITHOUT a restriction on the RSI
+def rsi_crosses_nocap(rsi, sma):
+    #print(rsi)
+    ret = len(rsi)*[0]
+
+
+    for i in range(len(rsi)-1):
+
+        if rsi[i] >= sma[i] and rsi[i+1] <= sma[i+1]:
+            ret[i+1] = 1
+            last_trade_rsi = rsi[i+1]
+        elif rsi[i] <= sma[i] and rsi[i+1] >= sma[i+1] and rsi[i+1] > 50:
+            ret[i+1] = -1
+            last_trade_rsi = rsi[i+1]
+        else:
+            ret[i+1] = 0
+
+
+
+    return np.array(ret)
+
+
+def rsi_crosses_nocap_relative_dist(rsi, sma, d, rsi_len):
+    #print(rsi)
+    ret = 2*rsi_len*[0]
+    ret.append(0)
+
+    last_trade_rsi = -1
+
+    for i in range(2*rsi_len, len(rsi)-1):
+
+        if last_trade_rsi == -1:
+            if rsi[i] <= sma[i] and rsi[i+1] >= sma[i+1]:
+                ret.append(1)
+                last_trade_rsi = rsi[i+1]
+            elif rsi[i] >= sma[i] and rsi[i+1] <= sma[i+1]:
+                ret.append(-1)
+                last_trade_rsi = rsi[i+1]
+            else:
+                ret.append(0)
+        else:
+            if rsi[i] <= sma[i] and rsi[i+1] >= sma[i+1] and rsi[i+1] <= last_trade_rsi*(1-d):
+                ret.append(1)
+                last_trade_rsi = rsi[i+1]
+            elif rsi[i] >= sma[i] and rsi[i+1] <= sma[i+1] and rsi[i+1] >= last_trade_rsi*(1+d):
+                ret.append(-1)
+                last_trade_rsi = rsi[i+1]
+            else:
+                ret.append(0)
+
+
+
+    return np.array(ret)
+
+
+# Generated Stochastic-Relative Strength signals WITHOUT a restriction on the RSI
+def stochrsi_crosses_nocap(stochrsi, stochrsi_sma):
+    #print(rsi)
+    ret = [0]
+
+
+    for i in range(len(stochrsi)-1):
+
+        if stochrsi[i] <= stochrsi_sma[i] and stochrsi[i+1] >= stochrsi_sma[i+1]:
+            ret.append(1)
+            last_trade_rsi = stochrsi[i+1]
+        elif stochrsi[i] >= stochrsi_sma[i] and stochrsi[i+1] <= stochrsi_sma[i+1]:
+            ret.append(-1)
+            last_trade_rsi = stochrsi[i+1]
+        else:
+            ret.append(0)
+
+    return np.array(ret)
+
+
+###### not yet finished Bollinger-Band signals
+def rsi_crosses_bb(data):
+    #print(rsi)
+    ret = []
+
+    sma = data["sma"]
+    rsi = data["rsi"]
+    bb_u = data["BB_UPPER"]
+    bb_m = data["BB_MIDDLE"]
+    bb_l = data["BB_LOWER"]
+
+    for i in range(len(rsi)-1):
+        if rsi[i] <= sma[i] and rsi[i+1] >= sma[i+1]:
+            ret.append(1)
+        elif rsi[i] >= sma[i] and rsi[i+1] <= sma[i+1]:
+            ret.append(-1)
+        else:
+            ret.append(0)
+
+    ret.append(0)
+
+    return np.array(ret)
+
+
+# Signal generator based on "rsi_crosses_nocap", BUT: Calculates the sum of differences between the RSI and its SMA
+# between each two OPPOSITE signals -> intuition is something like "integrating" the surface between the lines, between
+# the crossing points. Signals with such a sum below a threshold count as not valid enough and get cancelled.
+def rsi_diff_integration(data, diff_thres, u, l):
+
+    rsi = data["rsi"]
+    rsi_sma = data["rsi_sma"]
+    diff = [abs(x-y) for x, y in zip(rsi, rsi_sma)]
+
+    rsi_action = rsi_crosses_cap(rsi, rsi_sma, u, l)
+    action_diff = rsi_action.copy()
+    new_action = rsi_action.copy()
+
+    for i in range(len(rsi_action)):
+
+        cnt = 0
+
+        if abs(rsi_action[i]) == 1 and cnt == 0:
+
+            for j in range(i+1, len(rsi_action)):
+
+                if abs(rsi_action[j]) == 1 and cnt == 0:
+
+                    cnt = 1
+                    S = 0
+
+                    for k in range(i, j):
+
+                        for l in diff[i:j]:
+                            S += abs(l)
+                        action_diff[j] = (-1*rsi_action[j]*S)/(j-i) # avg of sum Method
+                        #action_diff[j] = (rsi_action[j] * S) # just sum Method
+
+    for i in range(len(new_action)):
+        if abs(action_diff[i]) < diff_thres:
+            new_action[i] = 0
+
+    return new_action
+
+# As above, based on stochastic RSI ("stochrsi_crosses_nocap")
+def stochrsi_diff_integration(data, diff_thres):
+
+    rsi = data["stochrsi"]
+    rsi_sma = data["stochrsi_sma"]
+    diff = [x-y for x, y in zip(rsi, rsi_sma)]
+
+    rsi_action = rsi_crosses_nocap(rsi, rsi_sma)
+    action_diff_avg = [float(rsi_action[i]) for i in range(len(rsi_action))]
+    action_diff_sum = [float(rsi_action[i]) for i in range(len(rsi_action))]
+    new_action = rsi_action.copy()
+
+    first_marker = -1 #Marker um erstes Signal wegen mangel an Infos ruszunehmen
+
+    for i in range(len(rsi_action)):
+
+        cnt = 0
+
+
+        if abs(rsi_action[i]) == 1 and cnt == 0:
+
+            if first_marker == -1: first_marker = i
+
+            for j in range(i+1, len(rsi_action)):
+
+                if abs(rsi_action[j]) == 1 and cnt == 0:
+
+                    S = 0
+
+                    for l in diff[i:j]:
+                        #print(l)
+                        S += l
+
+                    action_diff_avg[j] = (S)/(j-i) # avg Method
+                    action_diff_sum[j] = (S) # Sum Method
+
+    for i in range(len(new_action)):
+        if abs(action_diff_avg[i]) < diff_thres[0] or abs(action_diff_sum[i]) < diff_thres[1]:
+            new_action[i] = 0
+
+    action_diff_avg[first_marker] = 0
+    action_diff_sum[first_marker] = 0
+    new_action[first_marker] = 0
+
+    return new_action, action_diff_avg, action_diff_sum
+
+
+def sma4_crosses(data, sma4_len, sma_len):
+    sma = data["sma"]
+    close = data["close"]
+    sma_close = data["sma_close"]
+    sma_open = data["sma_open"]
+    sma_high = data["sma_high"]
+    sma_low = data["sma_low"]
+
+    sma4_diff = data["sma4_diff"]
+    sma4_mean = data["sma4_mean"]
+    sma4_diff_norm = data["sma4_diff_norm"]
+    sma4_diff_derivative_sma = data["sma4_diff_derivative_sma"]
+
+    ret = len(sma)*[0]
+
+
+    for i in range(max(sma4_len, sma_len), len(sma)-1):
+
+        ma_i = max(sma_close[i], sma_open[i], sma_high[i], sma_low[i])
+        mi_i = min(sma_close[i], sma_open[i], sma_high[i], sma_low[i])
+        ma_p1 = max(sma_close[i+1], sma_open[i+1], sma_high[i+1], sma_low[i+1])
+        mi_p1 = min(sma_close[i+1], sma_open[i+1], sma_high[i+1], sma_low[i+1])
+
+
+        #if sma[i] <= mi_i and sma[i+1] >= mi_p1 and sma4_diff_norm[i+1] > -1:
+        #    ret[i+1] = -1
+        #elif sma[i] >= ma_i and sma[i+1] <= ma_p1 and sma4_diff_norm[i+1] != 1000:
+              #and sma4_diff_norm[i+1] <= 5):
+        #    ret[i+1] = 1
+        #elif close[i] <= mi and close[i + 1] >= mi and sma4_diff_norm[i + 1] > 1.1*np.mean(sma4_diff_norm):
+        #    ret.append(-1)
+
+        if sma[i] <= mi_i and sma[i+1] >= mi_p1 and sma4_diff_norm[i+1] > 0:
+            ret[i+1] = 1
+        elif sma[i] >= ma_i and sma[i+1] <= ma_p1 and sma4_diff_norm[i+1] != 1000:
+              #and sma4_diff_norm[i+1] <= 5):
+            ret[i+1] = -1
+        #elif close[i] <= mi and close[i + 1] >= mi and sma4_diff_norm[i + 1] > 1.1*np.mean(sma4_diff_norm):
+        #    ret.append(-1)
+
+        else:
+            ret[i+1] = 0
+
+
+    return np.array(ret)
+
+
+
+
+
+
+
+
+######### BACKTESTING FUNCTIONS
+
+
+# I also experimented with other allocation methods, but this frist one was the most straight forward.
+# Inputs are a starting value of money, split = number of maximum positions on which the money is equally spread via
+# signals following another,percentual maximum-loss restriction and a maximum of ticks a position will be open
+def backtest_moneyinput(data, money=100, split=1, loss_limit_per=0, max_ticks=0):
+
+    # loss_limit_per=0 means no restriction of losses
+    # max_ticks=0 means theoretically forever opened positions
+
+    long = []
+    short = []
+    balance = 0
+    end_position_balance = 0
+    m = money
+    equity_curve = []
+    returns_per_trade = []
+
+    print("[Day] [Open LONG Positions] [Open SHORT Positions] [Current Profits] [Current Asset Price]")
+
+    for i in range(len(data["Signal"])):
+
+        A = data["Signal"][i]
+        p = data["close"][i]
+
+
+        if A == 1:
+
+            for j in range(len(short)):
+                change = ((short[j]-p)/short[j])*(money/split)
+                balance += change
+                returns_per_trade.append(change)
+
+
+            m += len(short) * (money / split)
+            short = []
+
+            if len(short) + len(long) < split:
+                long.append(p)
+                m -= money/split
+
+
+            print("[",i,"]", long, short, balance, p)
+
+        elif A == -1:
+
+            for j in range(len(long)):
+                change = (((p-long[j])/long[j])*(money/split))
+                balance += change
+                returns_per_trade.append(change)
+
+            m += len(long) * money / split
+            long = []
+
+            if len(short) + len(long) < split:
+                short.append(p)
+                m -= money / split
+
+            print("[",i,"]", long, short, balance, p)
+
+        elif loss_limit_per!=0: ###   Loss-Limit
+
+            for j in range(len(long)):
+                change = ((p-long[j])/long[j])*(money/split)
+                if change <= -loss_limit_per*money/split:
+                    balance += change
+                    returns_per_trade.append(change)
+                    del long[j]
+
+            for j in range(len(short)):
+                change = ((short[j]-p)/short[j])*(money/split)
+                if change <= -loss_limit_per*money/split:
+                    balance += change
+                    returns_per_trade.append(change)
+                    del short[j]
+
+        elif max_ticks != 0 and A == 0 and split == 1: ###   Maximum an Ticks einer Position, nur bei einer Position gleichzeitig
+
+            cnt = 1
+
+            for k in range(i, 0, -1):
+
+                if data["Signal"][k] == 0:
+                    cnt += 1
+                    continue
+                elif abs(data["Signal"][k]) == 1 and cnt >= max_ticks:
+
+                    if len(long) > 0:
+                        change = ((p-long[0])/long[0])*(money/split)
+                        balance += change
+                        returns_per_trade.append(change)
+                        del long[0]
+                    elif len(short) > 0:
+                        change = ((p - short[0]) / short[0]) * (money / split)
+                        balance += change
+                        returns_per_trade.append(change)
+                        del short[0]
+                    k = 0
+
+
+        equity_curve.append(money+balance)
+
+
+    for j in range(len(short)):
+        end_position_balance += ((short[j]-p)/short[j])*(money/split)
+
+    for j in range(len(long)):
+        end_position_balance += ((p-long[j])/long[j])*(money/split)
+
+
+
+
+    return balance, end_position_balance, equity_curve, returns_per_trade
+
+
+# This backtesting function opens a position of one contract/share/... on every signal, as long as the sum of money
+# invested is lower than the passed maximum value "max"
+def backtest_1persignal(data, max):
+
+    long = []
+    short = []
+    balance = 0
+    end_position_balance = 0
+
+    print("[Day] [Open LONG Positions] [Open SHORT Positions] [Current Profits] [Current Asset Price]")
+
+    if max < math.inf:
+        equity_curve = [max]
+    else:
+        equity_curve = [0]
+
+    for i in range(len(data["Signal"])):
+
+        A = data["Signal"][i]
+        p = data["close"][i]
+
+
+
+        if A == 1:
+
+            for j in range(len(short)):
+                balance += short[j]-p
+            short = []
+
+            if sum(long) <= max:
+                long.append(p)
+
+            print("[",i,"]", long, short, balance, p)
+
+        elif A == -1:
+
+            for j in range(len(long)):
+                balance += p-long[j]
+            long = []
+
+            #if sum(short) <= max:
+                #short.append(p)
+
+            print("[",i,"]", long, short, balance, p)
+
+        equity_curve.append(equity_curve[0]+balance)
+
+
+    for j in range(len(short)):
+        end_position_balance += short[j]-p
+
+    for j in range(len(long)):
+        end_position_balance += p-long[j]
+
+    del equity_curve[0]
+
+    return balance, end_position_balance, equity_curve
+
+
+# "backtest_allinever" works similar to "backtest_moneyinput", but on every position opened
+# all of the available money is invested
+def backtest_allinever(data, money):
+
+    long = []
+    short = []
+    balance = money
+    end_position_balance = 0
+    current_trade = 0
+    cnt_trades = 0
+    equity_curve = []
+    print("[Day] [Open LONG Positions] [Open SHORT Positions] [Current Profits] [Current Asset Price]")
+
+    for i in range(len(data["Signal"])):
+
+        A = data["Signal"][i]
+        p = data["close"][i]
+
+
+        if A == 1:
+            cnt_trades += 1
+            for j in range(len(short)):
+                balance = (1+abs(1-(p/short[j])))*current_trade
+            short = []
+
+            if len(long)+len(short) < 1:
+                long.append(p)
+                current_trade = balance
+                balance = 0
+
+            print("[",i,"]", long, short, current_trade-money, p)
+
+        elif A == -1:
+            cnt_trades += 1
+            for j in range(len(long)):
+                balance = ((current_trade)/long[j])*p
+            long = []
+
+            if len(long)+len(short) < 1:
+                short.append(p)
+                current_trade = balance
+                balance = 0
+
+            print("[",i,"]", long, short, current_trade-money, p)
+
+        if cnt_trades == 0:
+            equity_curve.append(money)
+        else:
+            equity_curve.append(current_trade)
+
+
+    balance = current_trade
+
+    for j in range(len(short)):
+        end_position_balance = (1+abs(1-(p/short[j])))*current_trade-balance
+
+    for j in range(len(long)):
+        end_position_balance = (current_trade/long[j])*p-balance
+
+
+    return balance-money, end_position_balance, equity_curve
+
+
+def intraday_open_close(data1, data2, startkapital, reinv):
+
+    S = []
+    equity_curve = [startkapital]
+
+    if reinv == False:
+
+        for i in range(0, len(data1)):
+            q = (data1[i] / data2[i])-1
+            S.append(q)
+            equity_curve.append(equity_curve[i]+q)
+            print(equity_curve)
+
+        return startkapital * (1+sum(S)), 0, equity_curve, S
+
+
+    elif reinv == True:
+
+        for i in range(0, len(data1)):
+            q = (data1[i] / data2[i])-1
+            S.append(q)
+            equity_curve.append(equity_curve[i]*(1+q))
+            #print(equity_curve)
+        return equity_curve[-1], 0, equity_curve, S
+
+
+
+
+######### HELP FUNCTIONS
+normalize = lambda l, f : [i/float(f) for i in l]
+
+array_trafo_dim1zuDIM1 = lambda l : [i[0] for i in l]
+
+def hochk_neg(x,k):
+    if x>=0: return pow(x,k)
+    else: return pow(x,k-1)*(-x)
+
+
+
+
+######### VISUALIZATION
+
+def scenario_visu(data, backtest, m, M, rsi_len,
+rsi_sma_len,
+rsi_smooth_len,
+sma_len,
+bollinger_len,
+diff_thres,
+sym,
+start,
+end):
+
+    ###     Plot Definition
+    fig = plt.figure(figsize=(14, 18))
+    gs = GridSpec(3, 1, height_ratios=[2, 2, 1])
+
+    # Subplots definieren
+    ax1 = fig.add_subplot(gs[0, 0])  # Upper Plot
+    ax2 = fig.add_subplot(gs[1, 0])  # Middle Plot
+    ax3 = fig.add_subplot(gs[2, 0])  # Lower Plot
+
+    ax1.plot(data['close'])
+    # axs[0].plot(data['Open'][m:M])
+    # axs[0].plot(data['High'][m:M])
+    # axs[0].plot(data['Low'][m:M])
+    #ax1.plot(data["sma"])
+    # axs[0].plot(data["sma_close"][m:M])
+    # axs[0].plot(data["sma_open"][m:M])
+    # axs[0].plot(data["sma_high"][m:M])
+    # axs[0].plot(data["sma_low"][m:M])
+    # axs[0].plot(data["BB_UPPER"][m:M])
+    # axs[0].plot(data["BB_MIDDLE"][m:M])
+    # axs[0].plot(data["BB_LOWER"][m:M])
+    long = np.where(data["Signal"] == 1, data["close"], None)
+    short = np.where(data["Signal"] == -1, data["close"], None)
+
+    ax1.scatter(list(range(0, M - m)), long, color="green", zorder=3)
+    ax1.scatter(list(range(0, M - m)), short, color="red", zorder=4)
+
+    #ax1.legend(["Closing Price", str("SMA(Close, " + str(sma_len) + ")")], bbox_to_anchor=(1, 1), fontsize=20)
+    ax1.legend(["Closing Price", "Long Signal", "Short Signal"], bbox_to_anchor=(1, 1), fontsize=20)
+    ax1.tick_params(labelsize=20)
+
+    # axs[1].plot(data["rsi"][m:M])
+    ax2.plot(data["rsi_sma"], zorder=1)
+    ax2.plot(data["rsi_smooth"], zorder=2)
+    # axs[1].plot(data["stochrsi"][m:M])
+    # axs[1].plot(data["stochrsi_sma"][m:M])
+    # axs[2].plot(data["sma4_diff_norm_sma"][m:M])
+    # axs[1].plot(data["ATR"][m:M])
+    # axs[1].plot(data["ATR_sma"][m:M])
+
+    long = np.where(data["Signal"] == 1, data["rsi_sma"], None)
+    short = np.where(data["Signal"] == -1, data["rsi_sma"], None)
+
+    ax2.scatter(list(range(0, M - m)), long, color="green", zorder=3)
+    ax2.scatter(list(range(0, M - m)), short, color="red", zorder=4)
+
+    ax2.legend(["RSI" + str(rsi_len), "SMA(RSI"+ str(rsi_len)+ ", " + str(rsi_sma_len) + ")", "Long Signal", "Short Signal"], bbox_to_anchor=(1.35, 1),
+               fontsize=20)
+    #ax2.hlines(y=50, xmin=0, xmax=M, color="black")
+    ax2.tick_params(labelsize=20)
+
+
+    # Simple plotting of signals -> 0 = nothing, 1 = go long, -1 = go short
+    # axs[2].scatter(list(range(m,M)), data["Signal"][m:M], color="red")
+
+
+    # axs[1].axhline(y=stat.median(data["sma4_diff_norm"])*1.12, color='b', linestyle='-')
+    # axs[1].axhline(y=u, color='b', linestyle='-')
+    # axs[1].axhline(y=l, color='b', linestyle='-')
+    # axs[1].legend(["rsi_sma", "rsi_smooth"])
+
+    # axs[2].plot(stochrsi_avg[m:M])
+    # axs[3].plot(stochrsi_sum[m:M], color="b")
+
+    # axs[2].plot(data["sma4_diff_derivative"][m:M])
+    # axs[3].plot(data["sma4_diff_derivative_sma"][m:M])
+    # axs[3].axhline(y=-0.7, color='b', linestyle='-')
+    # axs[3].axhline(y=0.7, color='b', linestyle='-')
+
+
+    ax3.plot(backtest[2])
+    ax3.legend(["Equity Curve"], bbox_to_anchor=(1, 1), fontsize=20)
+    ax3.tick_params(labelsize=20)
+    # print([i for i in backtest[3] if i >= 0])
+    # print([i for i in backtest[3] if i < 0])
+
+
+    """
+    last = 0
+    for i in range(1, len(data["Signal"])):
+
+        if data["Signal"][i] == 0:
+            continue
+        elif data["Signal"][i] == 1 and last in [0, -1]:
+            ax1.axvline(x=i, color="green", linestyle="--", linewidth=0.5)
+            ax2.axvline(x=i, color="green", linestyle="--", linewidth=0.5)
+            last = 1
+
+        elif data["Signal"][i] == -1 and last in [0, 1]:
+            ax1.axvline(x=i, color="red", linestyle="--", linewidth=0.5)
+            ax2.axvline(x=i, color="red", linestyle="--", linewidth=0.5)
+            last = -1
+
+        else:
+            continue
+    """
+
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,231 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from finta import TA as ta
+import yfinance as yf
+from pandas._config import display
+import statistics as stat
+import Backtesting_Methods as meth
+import math
+from matplotlib.gridspec import GridSpec
+
+## Tickers
+#Gold Future: GC=F
+#Soybeans Future: ZS=F
+#GBPJPY
+#Natural Gas Future: NG=F
+
+
+
+
+
+######      Scenario specifications
+
+#Simple Moving Average (SMA)
+sma_length = 3
+sma4_length = 8
+
+#Relative Strengh Index (RSI)
+rsi_length = 7
+rsi_sma_length = 3
+rsi_smooth_length = 7       # "Smooth" is here just a second SMA on the RSI
+
+#Stochastic RSI
+stochrsi_length = rsi_length
+stochrsi_sma_length = rsi_sma_length
+
+#Bollinger Bands
+bollinger_length = 4
+
+#Threshold for "integration" filters
+diff_threshold = 1
+diff_threshold_tupel = (0.01, 0.01)
+
+# Asset and time frame
+symbol = "GC=F"
+start_date = "2024-01-01"
+end_date = "2024-12-31"
+
+
+
+
+######      Definition of a function that generates signals and backtests strategy based on passed specifications
+def szenario(
+rsi_len = rsi_length,
+rsi_sma_len = rsi_sma_length,
+rsi_smooth_len = rsi_smooth_length,
+stochrsi_len = stochrsi_length,
+stochrsi_sma_len = stochrsi_sma_length,
+sma_len = sma_length,
+sma4_len = sma4_length,
+bollinger_len = bollinger_length,
+diff_thres_tu = diff_threshold_tupel,
+diff_thres = diff_threshold,
+sym = symbol,
+start = start_date,
+end = end_date):
+
+
+    # Data download via yFinance
+    data = pd.DataFrame(yf.download(sym, start=start, end=end))
+    data.columns = [col[0].lower() for col in data.columns]
+
+    ###     Adding technical indicators to the data (un-comment those who needed)
+
+    #Simple moving averages
+    data["sma"] = ta.SMA(data, sma_len, "close")
+    #data["sma_close"] = ta.SMA(data, sma4_len, "close")
+    #data["sma_open"] = ta.SMA(data, sma4_len, "open")
+    #data["sma_high"] = ta.SMA(data, sma4_len, "high")
+    #data["sma_low"] = ta.SMA(data, sma4_len, "low")
+
+    # Relative strengh index (RSI)
+    rsi = ta.RSI(data, rsi_len, "close")
+    data['rsi'] = rsi
+
+    # Different SMAs of the RSI
+    data["rsi_sma"] = ta.SMA(data, rsi_sma_len, "rsi")
+    data["rsi_smooth"] = ta.SMA(data, rsi_smooth_len, "rsi")
+
+    # Average True Range (ATR) -> measures volatility
+    #data["ATR"] = ta.ATR(data, 14)
+    #data["ATR_sma"] = ta.SMA(data, 14, column="ATR")
+
+    # Stochastic RSI
+    data["stochrsi"] = ta.STOCHRSI(data, 14, 14)
+    data["stochrsi_sma"] = ta.SMA(data, stochrsi_sma_length, "stochrsi")
+    data = data.fillna({"stochrsi": 0})
+
+    # Open-to-Low an open-to-high ratios on daily basis
+    #data["%opentoLow"] = [x / y for (x, y) in zip(data["Low"], data["Open"])]
+    #data["%opentoHigh"] = [x / y for (x, y) in zip(data["High"], data["Open"])]
+
+
+
+    #data["sma4_diff"] = [max(a,b,c,d)-min(a,b,c,d) for (a,b,c,d) in zip(data["sma_close"], data["sma_open"], data["sma_high"], data["sma_low"])]
+    #data["sma4_mean"] = [stat.mean((a, b, c, d)) for (a, b, c, d) in zip(data["sma_close"], data["sma_open"], data["sma_high"], data["sma_low"])]
+    #data["sma4_diff_norm"] = [a-np.mean(data["sma4_diff"]) for (a, b) in zip(data["sma4_diff"], data["sma4_mean"])]
+    #data["sma4_diff_norm_sma"] = ta.SMA(data, 2, "sma4_diff_norm")
+    #h = len(data["sma4_diff"])*[0]
+    #h[1:] = ([(data["sma4_diff"][i] - data["sma4_diff"][i-1]) for i in range(1, len(data["sma4_diff"]))])
+    #data["sma4_diff_derivative"] = h
+    #data["sma4_diff_derivative"].fillna(0, inplace=True)
+    #data["sma4_diff_derivative_sma"] = ta.SMA(data, 7, "sma4_diff_derivative")
+
+    # Bollinger Bands
+    #bollinger = ta.BBANDS(data, bollinger_len, column="close")
+    #data = data.join(bollinger)
+
+    # Replace NaN values with 0
+    data.fillna(0, inplace=True)
+
+    m = rsi_len     # Earlierst plottet data point -> needed because early datapoints are depending on indicator not usable yet (here RSI-length)
+    M = len(data)    # Latest used data points
+
+    u = 50          # Upper minimum bound for RSI
+    l = 50          # Lower maximum bound for RSI
+
+
+    #######     Generation of signals -> Method to use to be selected
+
+#Diese für Portfolio    #Basic RSI-cross based signals with restriction of upper/lower RSI-bounds
+    #data["Signal"] = meth.rsi_crosses_cap(data["rsi"][:], data["rsi_sma"][:], u, l)
+
+    #Basic RSI-cross based signals without upper/lower bound
+    data["Signal"] = meth.rsi_crosses_nocap(data["rsi"][:], data["rsi_sma"][:])
+
+    #Smoothened RSI-cross based signals with restriction of upper/lower RSI-bounds
+    #data["Signal"] = meth.rsi_crosses_cap(data["rsi_smooth"][:], data["rsi_sma"][:], u, l)
+
+    #Diese für Portfolio    #RSI based signal without upper/lower bounds with Difference-integration-filter
+    #data["Signal"] = meth.rsi_diff_integration(data, diff_thres, u, l)
+
+    #Stochastic RSI based Action
+    #data["Signal"] = meth.stochrsi_crosses_nocap(data["stochrsi"], data["stochrsi_sma"])
+
+    #Stochastic RSI based Action with Difference-integration-filter
+    #data["Signal"], stochrsi_avg, stochrsi_sum= meth.stochrsi_diff_integration(data, diff_thres_tu)
+
+    #Bollinger based signals
+    #data["Signal"] = meth.rsi_crosses_nocap(data["rsi_smooth"][:], data["rsi_sma"][:]) -> noch nicht implementiert
+
+    # SMA4 cross Signal
+    #data["Signal"] = meth.sma4_crosses(data, sma4_len, sma_len)
+
+
+    # Reindexing data of proper length
+    data = data.iloc[m:M]
+    data["index"] = list(range(0, len(data)))
+    data.set_index("index", inplace=True)
+
+
+
+    ######      Backtesting the strategy
+    backtest = meth.backtest_moneyinput(data, money=100, split=1, loss_limit_per=0, max_ticks=0)
+    #backtest = meth.backtest_1persignal(data, max=10000)
+    #backtest = meth.backtest_allinever(data, money=100)
+
+
+    ######      Call the visualization of the scenario
+    meth.scenario_visu(data, backtest, m, M, rsi_len = rsi_len,
+        rsi_sma_len = rsi_sma_len,
+        rsi_smooth_len = rsi_smooth_len,
+        sma_len = sma_len,
+        bollinger_len = bollinger_len,
+        diff_thres = diff_thres,
+        sym = symbol,
+        start = start_date,
+        end = end_date)
+
+    return backtest[0], backtest[1]
+
+
+
+######      Single Test
+
+scen = szenario(
+rsi_len = rsi_length,
+rsi_sma_len = rsi_sma_length,
+rsi_smooth_len = rsi_smooth_length,
+sma_len = sma_length,
+bollinger_len = bollinger_length,
+diff_thres = diff_threshold,
+sym = symbol,
+start = start_date,
+end = end_date)
+
+print("Realized profits:\t\t\t\t\t\t\t\t", scen[0])
+print("Realized profits + balance of open positions:\t", scen[0]+scen[1])
+
+
+
+
+######      Multi Test
+"""
+
+results = pd.DataFrame({'rsi': [], 'threshold': [], 'start_date' : [], "end_date" : [], 'return': []})
+st = np.arange(2010,2024)
+
+
+for r_l in range(7,8):
+
+    for thres in range(80,120, 10):
+
+        for start in st:
+
+            s = str(start)+"-01-01"
+            e = str(start+1)+"-01-01"
+
+            r = szenario(
+                rsi_len=r_l,
+                diff_thres=thres,
+                start=s,
+                end=e)
+
+            new_row = {"rsi":r_l, "threshold": thres, "start_date": s, "end_date": e, "return":r}
+            results = results._append(new_row, ignore_index=True)
+            print(new_row)
+"""
+
+
+
